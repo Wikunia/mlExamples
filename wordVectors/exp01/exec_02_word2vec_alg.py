ab7dcddc22838715542c9769b2d186b1f242d187
marc <marc>
2016-01-03 00:03:42 +0100

    additional tests

--- wordVectors/exp01/exec_02_word2vec_alg.py
+++ wordVectors/exp01/exec_02_word2vec_alg.py
@@ -22,12 +22,16 @@ with codecs.open('output/sentences_list.json', 'r', encoding='utf-8') as f:
 
 print "Creating the word vectors...\n"
 # Initialize and train the WordVector model
-num_features = 300    # Word vector dimensionality, size of the numpy-vector
+num_features = 300    # Word vector dimensionality, size of the numpy-vector, size of the neural-network-layers
 min_word_count = 40   # Minimum word count, to filter very rarely used words
 context = 10          # Context window size
 downsampling = 1e-3   # Downsample setting for frequent words, to filter stopwords
 
-
+"""
+ Train word2vec on the sentences.
+ The first pass collects words and their frequencies to build an internal dictionary tree structure.
+ The second pass trains the neural model.
+"""
 model = word2vec.Word2Vec(sentences, workers=2, \
             size=num_features, min_count = min_word_count, \
             window = context, sample = downsampling)
--- wordVectors/exp01/exec_03_prediction.py
+++ wordVectors/exp01/exec_03_prediction.py
@@ -3,9 +3,9 @@
 from gensim.models import word2vec
 
 
-###########################################################################
-# Restore word2vector model
-###########################################################################
+######################################################################################
+# Restore word2vector model (trained model with some semantic understanding of words,)
+######################################################################################
 
 model = word2vec.Word2Vec()
 model = model.load("output/IMDB_300features_40minwords_10context.cnpy")
@@ -38,10 +38,37 @@ print ("doesnt_match(paris, berlin, london, austria) : %s \n" % (result))
 ##########################################################################
 
 """
- The "doesnt_match" function will try to deduce which word in a set is most dissimilar from the others:
+ The "most_similar" function will print the word cluster of similarity
 """
 print("\n###################### most_similar   ######################\n")
 
 print ("most_similar(man) :\n %s \n" % (model.most_similar("man")))
 print ("most_similar(queen) :\n %s \n" % (model.most_similar("queen")))
 print ("most_similar(awful) :\n %s \n" % (model.most_similar("awful")))
+print ("most_similar(positive=[woman, king], negative=[man], topn=1) :\n %s \n"
+        % (model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)))
+
+
+##########################################################################
+#Make predictions: similarity()  distance between words
+##########################################################################
+
+"""
+ The "similarity" function will print the distance between word similarity
+"""
+
+print("\n###################### similarity   ######################\n")
+
+print ("model.similarity('woman', 'man') :\n %s \n" % (model.similarity('woman', 'man')))
+
+##########################################################################
+#Raw output
+##########################################################################
+
+"""
+ raw NumPy vector of a word
+"""
+#print("\n###################### Raw output   ######################\n")
+
+#print ("model['man'] : \n %s \n \n" % (model['man'])) # Individual word vectors can be accessed by vocabulary
+#print ("model['man'] : \n %s \n \n" % (model.syn0[1])) # Or accessed by index
